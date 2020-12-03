ab6a6015b6e8b699fadd6251bf359f8cb6fbf3a8
Aditya Shriwastava <adityashriwastava7@gmail.com>
2020-12-03 11:48:13 +0530

    Add ps1_1e

--- problem set with solution/ps1/src/run.py
+++ problem set with solution/ps1/src/run.py
@@ -7,3 +7,13 @@ p01b(train_path='../data/ds1_train.csv',
 p01b(train_path='../data/ds2_train.csv',
      eval_path='../data/ds2_valid.csv',
      pred_path='output/p01b_pred_2.txt')
+
+from p01e_gda import main as p01e
+
+p01e(train_path='../data/ds1_train.csv',
+     eval_path='../data/ds1_valid.csv',
+     pred_path='output/p01e_pred_1.txt')
+
+p01e(train_path='../data/ds2_train.csv',
+     eval_path='../data/ds2_valid.csv',
+     pred_path='output/p01e_pred_2.txt')
