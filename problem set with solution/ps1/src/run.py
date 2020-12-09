1c809e4f96efb969cf1da2c75edd553ae3157246
Aditya Shriwastava <adityashriwastava7@gmail.com>
2020-12-09 15:51:52 +0530

    add ps1_2{b,c,d,e}

--- problem set with solution/ps1/src/run.py
+++ problem set with solution/ps1/src/run.py
@@ -17,3 +17,9 @@ p01e(train_path='../data/ds1_train.csv',
 p01e(train_path='../data/ds2_train.csv',
      eval_path='../data/ds2_valid.csv',
      pred_path='output/p01e_pred_2.txt')
+
+from p02cde_posonly import main as p02
+p02(train_path='../data/ds3_train.csv',
+    valid_path='../data/ds3_valid.csv',
+    test_path='../data/ds3_test.csv',
+    pred_path='output/p02X_pred.txt')
