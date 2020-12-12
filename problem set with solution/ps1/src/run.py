f502182c72d149e60dd54c7f51d6db883a0cd97f
Aditya Shriwastava <adityashriwastava7@gmail.com>
2020-12-12 13:30:47 +0530

    add p03d_poisson

--- problem set with solution/ps1/src/run.py
+++ problem set with solution/ps1/src/run.py
@@ -23,3 +23,9 @@ p02(train_path='../data/ds3_train.csv',
     valid_path='../data/ds3_valid.csv',
     test_path='../data/ds3_test.csv',
     pred_path='output/p02X_pred.txt')
+
+from p03d_poisson import main as p03
+p03(lr=1e-7,
+    train_path='../data/ds4_train.csv',
+    eval_path='../data/ds4_valid.csv',
+    pred_path='output/p03d_pred.txt')
